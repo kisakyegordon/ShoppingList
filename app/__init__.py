b216f63e5115153e1a6296f8f00dc7415b1da765
kisakyegordon <gordon56692@gmail.com>
2017-09-28 00:56:02 +0300

    Main Functionality File

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,10 @@ db = SQLAlchemy()
 def create_app(config_name):
     from app.models import ShoppingList, User
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config['config_name'])
-    app.config.from_pyfile('config.py')
+    bcrypt = Bcrypt(app)
+
+    app.config.from_object(app_config[config_name])
+    # app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
