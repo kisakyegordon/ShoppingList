f7b4cfd13555737afa46085ae9c4396f45a57c1a
el gordo <elgordo@kisakyes-MacBook-Pro.local>
2017-10-07 22:25:26 +0300

    Main Application File

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS')
-app = create_app(config_name)
+# config_name = os.getenv('APP_SETTINGS')
+# app = create_app(config_name)
+app = create_app("development")
 
 if __name__ == '__main__':
     app.run()
