51ba5796da3ea47b205636779baaa7ff37f40e4b
el gordo <elgordo@kisakyes-MacBook-Pro.local>
2017-10-11 01:43:36 +0300

    Added The BlackList Model

--- app/models.py
+++ app/models.py
@@ -2,7 +2,10 @@ from app import db
 from flask_bcrypt import Bcrypt
 from flask import current_app
 import jwt
+# import datetime
 from datetime import datetime, timedelta
+
+
 
 
 class User(db.Model):
@@ -42,9 +45,28 @@ class User(db.Model):
                 current_app.config.get('SECRET'),
                 algorithm='HS256'
             )
+
             return jwt_string
         except Exception as e:
             return str(e)
+
+    
+    # def crush_token(self, user_id):
+    #     try:
+    #         payload = {
+    #             'exp' : datetime.utcnow() + timedelta(minutes=0),
+    #             'iat' : datetime.utcnow(),
+    #             'sub' : user_id
+    #         }
+
+    #         jwt_string = jwt.encode(
+    #             payload,
+    #             current_app.config.get('SECRET'),
+    #             algorithm='HS256'
+    #         )
+    #         return jwt_string
+    #     except Exception as e:
+    #         return str(e)
 
     @staticmethod
     def decode_token(token):
@@ -58,6 +80,24 @@ class User(db.Model):
         except jwt.InvalidTokenError:
 
             return "Invalid token. Please regidter or login"
+
+    # @staticmethod
+    # def decode_token(token):
+    #     """
+    #     Validates the auth token
+    #     """
+    #     try:
+    #         payload = jwt.decode(token, current_app.config.get('SECRET'))
+    #         is_blacklisted_token = BlacklistToken.check_blacklist(token)
+    #         if is_blacklisted_token:
+    #             return 'Token blacklisted. Please log in again.'
+    #         else:
+    #             return payload['sub']
+    #     except jwt.ExpiredSignatureError:
+    #         return 'Signature expired. Please log in again.'
+    #     except jwt.InvalidTokenError:
+    #         return 'Invalid token. Please log in again.'
+
 
 class ShoppingList(db.Model):
 
@@ -78,9 +118,9 @@ class ShoppingList(db.Model):
         db.session.commit()
 
 
-    @staticmethod
-    def get_all():
-        return ShoppingList.query.all()
+    # @staticmethod
+    # def get_all():
+    #     return ShoppingList.query.all()
 
 
     def delete(self):
@@ -115,5 +155,38 @@ class ListItem(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+class BlacklistToken(db.Model):
+    """
+    Model for storing blacklisted tokens
+    """
+    
+    __tablename__ = 'blacklist'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklist_status = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklist_status = True
+        # self.blacklist_status = datetime.datetime.now()
+        # self.blacklist_status = datetime.now()
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
+
+    def save(self):
+        self.session.add(self)
+        self.session.commit()
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether token has been blacklisted
+
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+        else:
+            return False
 
         
