4c30fb13ccce86ec638068fc47bd5a14cb7507e4
el gordo <elgordo@kisakyes-MacBook-Pro.local>
2017-10-11 01:50:24 +0300

    Modified Configuration File

--- instance/config.py
+++ instance/config.py
@@ -5,13 +5,18 @@ class Config(object):
 
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    # SECRET = os.getenv('SECRET')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET = "very sensitive text"
+    SQLALCHEMY_DATABASE_URI = 'postgresql://kisakye:kisakye6@localhost/shoppinglist_db'
 
 
 class DevelopmentConfig(Config):
+    # FLASK_APP = "run.py"
+    Development = True
+    # SECRET = "very sensitive text"
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql://kisakye:kisakye6@localhost/shoppinglist_db'
 
 class TestingConfig(Config):
     Testing = True
