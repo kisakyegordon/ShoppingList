32b09c8ab50f1e1220ddbb579822f621675bd7e8
el gordo <elgordo@kisakyes-MacBook-Pro.local>
2017-10-16 12:37:26 +0300

    Modified Logout Functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,9 +2,8 @@ from . import auth_blueprint
 from flask_bcrypt import Bcrypt
 from flask.views import MethodView
 from flask import Blueprint, make_response, request, jsonify
-from app.models import User, BlacklistToken
-from flask import json, abort
 from app.models import User
+from flask import json, abort, request, jsonify
 
 
 class RegistrationView(MethodView):
@@ -19,7 +18,8 @@ class RegistrationView(MethodView):
 
                 email = post_data['email']
                 password = post_data['password']
-                user = User(email=email, password=password)
+                country_town = post_data['country_town']
+                user = User(email=email, password=password, country_town=country_town)
                 user.save()
 
                 response = {
@@ -35,7 +35,7 @@ class RegistrationView(MethodView):
                 return make_response(jsonify(response)), 401
         else:
             response = {
-                'message': 'User already exists. Please login.'
+                'message': 'User Already Exists. Please Login.'
             }
             return make_response(jsonify(response)), 202
 
@@ -49,6 +49,10 @@ class LoginView(MethodView):
             if user and user.password_is_valid(request.data['password']):
                 access_token = user.generate_token(user.id)
                 if access_token:
+
+                    user.token = access_token.decode()
+                    user.save()
+
                     response = {
                         'message': 'Successfully LoggedIn',
                         'access_token': access_token.decode()
@@ -66,31 +70,38 @@ class LoginView(MethodView):
             # }
             # return make_response(jsonify(response)), 500
 
-
 class LogoutView(MethodView):
     
-    def get(self):
+    def post(self):
         
-        try:
-            response = {
-                'message': 'You Were Logged Out Successfully',
-                'access_token': None
-            }
-            return make_response(jsonify(response)), 200
+        auth_header = request.headers.get("Authorization")
+        access_token = auth_header.split(" ")[1]
+        
+        if access_token:
+            user_id = User.decode_token(access_token)
+            if isinstance(user_id, int):
+                user_id = User.decode_token(access_token)
+                user = User.query.filter_by(id=user_id).first()
+                
+                if user:
+                    user.token = ''
+                    user.save()
+                
+                response = {
+                    'message': 'Logged Out Successfully'
+                }
 
-        except Exception as e:
-            response = {
-                'message': str(e)
-            }
-            return make_response(jsonify(response)), 401
+                return make_response(jsonify(response)), 200
 
 
 class ResetView(MethodView):
     
     def post(self):
         data = request.get_json()
-        email = data['email']
-        user = User.query.filter_by(email=email).first()
+        # email = data['email']
+        # user = User.query.filter_by(email=email).first()
+        country_town = data['country_town']
+        user = User.query.filter_by(country_town=country_town).first()
         if not user:
             return make_response(jsonify({'No User Found'})), 404
         user.password = Bcrypt().generate_password_hash(data['password']).decode()
@@ -120,7 +131,7 @@ auth_blueprint.add_url_rule(
 auth_blueprint.add_url_rule(
     '/auth/logout',
     view_func=logout_view,
-    methods=['GET']
+    methods=['POST']
 )
 
 auth_blueprint.add_url_rule(
