51b1fcf2f50160463cdd797b76c17f2d3670f947
el gordo <elgordo@kisakyes-MacBook-Pro.local>
2017-10-16 12:38:08 +0300

    Improved User Model with token column

--- app/models.py
+++ app/models.py
@@ -2,11 +2,7 @@ from app import db
 from flask_bcrypt import Bcrypt
 from flask import current_app
 import jwt
-# import datetime
 from datetime import datetime, timedelta
-
-
-
 
 class User(db.Model):
 
@@ -15,13 +11,17 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
+    country_town = db.Column(db.String(200), nullable=False)
+    token = db.Column(db.String(300), nullable=True)
     lists = db.relationship("ShoppingList", order_by="ShoppingList.id", cascade="all, delete-orphan")
 
 
-    def __init__(self, email, password):
+    def __init__(self, email, password, country_town, token=None):
         """Initialize the user with email and password"""
 
         self.email = email
+        self.country_town = country_town
+        self.token = token
         self.password = Bcrypt().generate_password_hash(password).decode()
 
     def password_is_valid(self, password):
@@ -50,24 +50,6 @@ class User(db.Model):
         except Exception as e:
             return str(e)
 
-    
-    # def crush_token(self, user_id):
-    #     try:
-    #         payload = {
-    #             'exp' : datetime.utcnow() + timedelta(minutes=0),
-    #             'iat' : datetime.utcnow(),
-    #             'sub' : user_id
-    #         }
-
-    #         jwt_string = jwt.encode(
-    #             payload,
-    #             current_app.config.get('SECRET'),
-    #             algorithm='HS256'
-    #         )
-    #         return jwt_string
-    #     except Exception as e:
-    #         return str(e)
-
     @staticmethod
     def decode_token(token):
 
@@ -80,24 +62,6 @@ class User(db.Model):
         except jwt.InvalidTokenError:
 
             return "Invalid token. Please regidter or login"
-
-    # @staticmethod
-    # def decode_token(token):
-    #     """
-    #     Validates the auth token
-    #     """
-    #     try:
-    #         payload = jwt.decode(token, current_app.config.get('SECRET'))
-    #         is_blacklisted_token = BlacklistToken.check_blacklist(token)
-    #         if is_blacklisted_token:
-    #             return 'Token blacklisted. Please log in again.'
-    #         else:
-    #             return payload['sub']
-    #     except jwt.ExpiredSignatureError:
-    #         return 'Signature expired. Please log in again.'
-    #     except jwt.InvalidTokenError:
-    #         return 'Invalid token. Please log in again.'
-
 
 class ShoppingList(db.Model):
 
@@ -156,37 +120,36 @@ class ListItem(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-class BlacklistToken(db.Model):
-    """
-    Model for storing blacklisted tokens
-    """
+# class BlacklistToken(db.Model):
+#     """
+#     Model for storing blacklisted tokens
+#     """
     
-    __tablename__ = 'blacklist'
+#     __tablename__ = 'blacklist'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    token = db.Column(db.String(500), unique=True, nullable=False)
-    blacklist_status = db.Column(db.Boolean, nullable=False)
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     token = db.Column(db.String(500), unique=True, nullable=False)
+#     blacklist_status = db.Column(db.DateTime,  nullable=False)
 
-    def __init__(self, token):
-        self.token = token
-        self.blacklist_status = True
-        # self.blacklist_status = datetime.datetime.now()
-        # self.blacklist_status = datetime.now()
-    def __repr__(self):
-        return '<id: token: {}'.format(self.token)
+#     def __init__(self, token):
+#         self.token = token
+#         # self.blacklist_status = datetime.datetime.now()
+#         self.blacklist_status = datetime.now()
+#     def __repr__(self):
+#         return '<id: token: {}'.format(self.token)
 
-    def save(self):
-        self.session.add(self)
-        self.session.commit()
+#     def save(self):
+#         self.session.add(self)
+#         self.session.commit()
 
-    @staticmethod
-    def check_blacklist(auth_token):
-        # check whether token has been blacklisted
+#     @staticmethod
+#     def check_blacklist(auth_token):
+#         # check whether token has been blacklisted
 
-        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
-        if res:
-            return True
-        else:
-            return False
+#         res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+#         if res:
+#             return True
+#         else:
+#             return False
 
         
