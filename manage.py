7129156b8f468d91d12cecc32c3411fad5e684e2
kisakyegordon <gordon56692@gmail.com>
2017-10-24 19:46:57 +0300

    Modify manage.py file

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
+"""
+Manage model for handling database migrations and also automating tests
+"""
 import os
 import unittest
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
+# from app import models
 
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
@@ -14,6 +17,9 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
+    """
+    Test Automation
+    """
     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
